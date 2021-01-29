ea2850cf4a36a6f7a2b99dc3d82df2c27f8508f3
unknown <erichris@live.com.mx>
2021-01-28 22:35:48 -0600

    Random movement and UI Manager

---
+++ Assets/Scripts/Chris/SingletonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonManager : MonoBehaviour
+{
+    [HideInInspector]
+    public static SingletonManager Singleton;
+    public ScreenManager screenManager;
+
+    void Awake()
+    {
+        if(Singleton == null)
+        {
+            Singleton = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public void JustForTestingEvents()
+    {
+        Debug.Log("Triggered");
+    }
+}
