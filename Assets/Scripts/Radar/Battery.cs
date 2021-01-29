acadea846bb224c68e4c26b192992a4d3a87a4fd
Yo030 <seryrak49@gmail.com>
2021-01-29 04:33:57 -0600

    Se agregaron los tesoros que se van a espawnear con peso de probabilidad.

---
+++ Assets/Scripts/Radar/Battery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    public float BatteryLife;
+    [SerializeField] private GameObject LoseUI;
+    private ThirdPersonController ThirdPersonControllerScript;
+
+    private void Start()
+    {
+        ThirdPersonControllerScript = FindObjectOfType<ThirdPersonController>();
+    }
+
+    private void Update()
+    {
+        if(BatteryLife <= 0)
+        {
+            ThirdPersonControllerScript.CanMove = false;
+            LoseUI.SetActive(true);
+        }
+    }
+
+    public void ReduceBatteryLife()
+    {
+        BatteryLife -= 1 * Time.deltaTime;
+    }
+}
