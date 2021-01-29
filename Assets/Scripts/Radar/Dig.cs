2a92ef919e5032845f5e71facd44d391c5aec919
Yo030 <seryrak49@gmail.com>
2021-01-28 20:41:04 -0600

    Encontrar Objetos
    Se agrego el escarvar objetos, y un UI simple

---
+++ Assets/Scripts/Radar/Dig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dig : MonoBehaviour
+{
+    [SerializeField] private GameObject DigUI;
+
+    public void ShowDigUI(bool _show)
+    {
+        DigUI.SetActive(_show);//Show UI
+    }
+
+    public void DiscoverTresure(GameObject _buriedobject)
+    {
+        Debug.Log("YOU JUST DIGGED!!!");
+        DigUI.SetActive(false);//Show UI
+        Destroy(_buriedobject);        
+    }
+}
+
--- Assets/Scripts/Radar/MetalDetector.cs
+++ Assets/Scripts/Radar/MetalDetector.cs
@@ -7,44 +7,79 @@ public class MetalDetector : MonoBehaviour
     [SerializeField] private float DepthDistance;
     [SerializeField] private LayerMask BuriedLayer;
     [SerializeField] private float MetalDetectorRadius;
-    [SerializeField] private int DivideCircleIn = 5;
+    [SerializeField] private int DivideCircleIn = 5; //2.5 with metal detecter 5 normal
     [SerializeField] private GameObject[] BuriedObjects;
     private GameObject ParentGameObject;
     [SerializeField] private GameObject AudioManager_GameObject;
     private AudioSource AudioSource_Component;
     private AudioManager AudioManager_Script;
 
-    private bool CloseToBuriedObject;
+    [SerializeField] private bool OnTopOfTresure;
+    private bool CanPlayFoundAudio = true;
 
+    [SerializeField] private Dig DigScript;
+    //private bool CanDig;
+    [SerializeField] private GameObject BuriedObjectBeneath;
 
     private void Start()
     {
         ParentGameObject = gameObject.transform.parent.gameObject;
         AudioSource_Component = AudioManager_GameObject.GetComponent<AudioSource>();
         AudioManager_Script = AudioManager_GameObject.GetComponent<AudioManager>();
+        DigScript = FindObjectOfType<Dig>();
+    }
+    
+    private void Update()
+    {
+        if(BuriedObjectBeneath != null)
+        {
+            DigScript.ShowDigUI(true);
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                DigScript.DiscoverTresure(BuriedObjectBeneath);
+            }
+        }
+        else
+        {
+            DigScript.ShowDigUI(false);
+        }
     }
 
     void FixedUpdate()
     {
-
         RaycastHit hit;
         if (Physics.Raycast(ParentGameObject.transform.position, transform.TransformDirection(Vector3.down), out hit, DepthDistance, BuriedLayer))
         {
             Debug.DrawRay(ParentGameObject.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.green);
+            OnTopOfTresure = true;
+            BuriedObjectBeneath = hit.transform.gameObject;
+            //CanDig = true;
         }
         else
         {
             Debug.DrawRay(ParentGameObject.transform.position, transform.TransformDirection(Vector3.down) * DepthDistance, Color.red);
+            if(OnTopOfTresure)
+            {
+                CanPlayFoundAudio = true;
+                OnTopOfTresure = false;
+                BuriedObjectBeneath = null;
+                //CanDig = false;
+            }
         }
+
 
         FindNearestBuriedObject();
     }
 
     private void OnDrawGizmos()
     {
-        // Draw a semitransparent blue cube at the transforms position
-        Gizmos.color = new Color(50, 100, 10, .5f);
-        Gizmos.DrawSphere(transform.position, MetalDetectorRadius);
+        float _devidedradius = MetalDetectorRadius / DivideCircleIn;
+        Gizmos.color = new Color(50, 100, 10, .2f);
+        for(int i=0; i< DivideCircleIn; i++)
+        {
+            Gizmos.DrawSphere(transform.position, _devidedradius * i);
+        }
     }
 
     private void FindNearestBuriedObject()
@@ -57,11 +92,6 @@ public class MetalDetector : MonoBehaviour
             {
                 Debug.DrawLine(this.transform.position, BuriedObjects[i].transform.position, Color.blue);
                 FoundObject(distancetoobject);
-                CloseToBuriedObject = true;
-            }
-            else
-            {
-                CloseToBuriedObject = true;
             }
         }
     }
@@ -69,40 +99,48 @@ public class MetalDetector : MonoBehaviour
     private void FoundObject(float _distncetoobject)
     {
         float _devidedradius = MetalDetectorRadius / DivideCircleIn;
-
-
-        if (_distncetoobject <= _devidedradius)
+        
+        if(OnTopOfTresure)
         {
-            Debug.Log("Bueried Obj is LESS then 1/5 Away");
-            PlayAudio(3f);
+            PlayAudio("Beet", 4.5f);
+            if (CanPlayFoundAudio)
+            {
+                AudioManager_Script.play("Correct_Beep", 1f);
+                CanPlayFoundAudio = false;
+            }
+        }
+        else if (_distncetoobject <= _devidedradius)
+        {
+            //Debug.Log("Bueried Obj is LESS then 1/5 Away");
+            PlayAudio("Beet", 3f);
         }
         else if (_distncetoobject <= _devidedradius * 2)
         {
-            Debug.Log("Bueried Obj is LESS then 2/5 Away");
-            PlayAudio(2.5f);
+            //Debug.Log("Bueried Obj is LESS then 2/5 Away");
+            PlayAudio("Beet", 2.5f);
         }
         else if (_distncetoobject <= _devidedradius * 3)
         {
-            Debug.Log("Bueried Obj is LESS then 3/5 Away");
-            PlayAudio(2f);
+            //Debug.Log("Bueried Obj is LESS then 3/5 Away");
+            PlayAudio("Beet", 2f);
         }
         else if (_distncetoobject <= _devidedradius * 4)
         {
-            Debug.Log("Bueried Obj is LESS then 4/5 Away");
-            PlayAudio(1.5f);
+            //Debug.Log("Bueried Obj is LESS then 4/5 Away");
+            PlayAudio("Beet", 1.5f);
         }
         else
         {
-            Debug.Log("Bueried Obj is MORE than 4/5 Away");
-            PlayAudio(1f);
+            //Debug.Log("Bueried Obj is MORE than 4/5 Away");
+            PlayAudio("Beet", 1f);
         }
     }
 
-    private void PlayAudio(float _pitch)
+    private void PlayAudio(string _soundname, float _pitch)
     {
-        if(!AudioSource_Component.isPlaying && CloseToBuriedObject)
+        if(!AudioSource_Component.isPlaying)
         {
-            AudioManager_Script.play("Beet", _pitch);
+            AudioManager_Script.play(_soundname, _pitch);
         }
     }
 }
