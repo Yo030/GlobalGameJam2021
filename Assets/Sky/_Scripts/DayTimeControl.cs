4ba9c92eb47faa2ce57449d9fe28a6f7548aed83
yosoykyukim <yosoykyukim@gmail.com>
2021-01-29 19:02:03 -0600

    Cielito e isla

---
+++ Assets/Sky/_Scripts/DayTimeControl.cs
@@ -0,0 +1,92 @@
+﻿
+using UnityEngine;
+
+public class DayTimeControl : MonoBehaviour {
+
+
+    DayTimeControlEngine engine = new DayTimeControlEngine();
+
+
+    public float dayLenghtInMinutes;
+    public int dayCount;
+
+    [SerializeField]
+    private float dayTimeHours;
+    [SerializeField]
+    private float dayTimeMinutes;
+
+    public GameObject moon;
+    public GameObject sun;
+
+    private float diffuseSpeedSun = .02f;
+    private float diffuseSpeedMoon = .02f;
+    private float time = 0f;
+    private float offsetSpeed;
+    private float textureOffset = 0f;
+
+    public Material material;
+
+    private void Start()
+    {
+        offsetSpeed = 1f / (dayLenghtInMinutes * 60);
+        material.SetTextureOffset("_MainTex", new Vector2(0, 0));
+    }
+
+    void Update () {
+
+        CountDays();
+
+        time += Time.deltaTime;
+
+        engine.CycleCompleted(ref time, textureOffset); //texture has completed a cycle so restart it
+        
+        TextureOffset();
+
+        if (textureOffset >= .7)
+        {
+            //day
+            turnSun();
+        } else if (textureOffset >= .2) {
+            //night
+            turnMoon();
+        }
+    }
+
+    private void TextureOffset()
+    {
+        material.SetTextureOffset("_MainTex", new Vector2(engine.ChangeTextureOffset(time, offsetSpeed), 0));
+        textureOffset = engine.ChangeTextureOffset(time, offsetSpeed);
+    }
+
+    private void turnMoon()
+    {
+        engine.TurnMoon(sun.transform.localScale.x, moon.transform.localScale.x, ref diffuseSpeedMoon, ref diffuseSpeedSun);
+        moon.transform.localScale += new Vector3(diffuseSpeedMoon, diffuseSpeedMoon, diffuseSpeedMoon);
+        sun.transform.localScale += new Vector3(diffuseSpeedSun, diffuseSpeedSun, diffuseSpeedSun);
+    }
+
+    private void turnSun()
+    {
+        engine.TurnSun(sun.transform.localScale.x, moon.transform.localScale.x, ref diffuseSpeedMoon, ref diffuseSpeedSun);
+        moon.transform.localScale += new Vector3(diffuseSpeedMoon, diffuseSpeedMoon, diffuseSpeedMoon);
+        sun.transform.localScale += new Vector3(diffuseSpeedSun, diffuseSpeedSun, diffuseSpeedSun);
+    }
+
+    
+    private void CountDays()
+    {
+        AddHours();
+        engine.CountDays(ref dayCount, ref dayTimeHours);
+    }
+
+    private void AddHours()
+    {
+        AddMinutesPerSecond();
+        engine.AddHours(ref dayTimeHours, ref dayTimeMinutes);
+    }
+
+    private void AddMinutesPerSecond()
+    {
+        dayTimeMinutes = engine.AddMinutesPerSecond(dayLenghtInMinutes, Time.deltaTime, dayTimeMinutes);
+    }
+}
---
+++ Assets/Sky/_Scripts/DayTimeControlEngine.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DayTimeControlEngine
+{
+    public float AddMinutesPerSecond(float _dayLengthInMinutes,float _deltaTime, float _dayTimeMinutes)
+    {
+        _dayTimeMinutes += (1440 / (_dayLengthInMinutes * 60)) * _deltaTime;
+
+        return _dayTimeMinutes;
+    }
+
+    public void AddHours(ref float _dayTimeHours, ref float _dayTimeMinutes)
+    {
+        if (_dayTimeMinutes >= 60)
+        {
+            _dayTimeHours++;
+            _dayTimeMinutes -= 60;
+        }
+    }
+
+    public void CountDays(ref int _dayCount, ref float _dayTimeHours)
+    {
+        if (_dayTimeHours >= 24)
+        {
+            _dayCount++;
+            _dayTimeHours -= 24;
+        }
+    }
+
+    public void TurnSun(float _sunScale, float _moonScale, ref float _diffuseSpeedMoon, ref float _diffuseSpeedSun)
+    {
+        if (_sunScale <= 1)
+        {
+            _diffuseSpeedSun = .02f;
+        }
+        else
+        {
+            _diffuseSpeedSun = 0f;
+        }
+        if (_moonScale > 0)
+        {
+            _diffuseSpeedMoon = -.02f;
+        }
+        else
+        {
+            _diffuseSpeedMoon = 0f;
+        }
+    }
+
+    public void TurnMoon(float _sunScale, float _moonScale, ref float _diffuseSpeedMoon, ref float _diffuseSpeedSun)
+    {
+
+        if (_sunScale > 0)
+        {
+            _diffuseSpeedSun = -.02f;
+        }
+        else
+        {
+            _diffuseSpeedSun = 0f;
+        }
+        if (_moonScale <= 1)
+        {
+            _diffuseSpeedMoon = .02f;
+        } else
+        {
+            _diffuseSpeedMoon = 0f;
+        }
+        
+    }
+
+    public float ChangeTextureOffset(float _time, float _offsetSpeed)
+    {
+        float offset = _time * _offsetSpeed;
+        return offset;
+    }
+    
+    public void CycleCompleted(ref float _time, float _textureOffset)
+    {
+        if (_textureOffset >= 1)
+        {
+            _time = 0;
+        }
+    }
+}
